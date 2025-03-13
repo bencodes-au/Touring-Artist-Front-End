b8d33b3b95443f45ee039fb5c01f0cc5a13b31c5
ben_gorman <bengorman1994@gmail.com>
2025-03-13 21:15:37 +1100

    fix: deployment fix

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API_BASE_URL = "https://touring-artist-back-end.onrender.com";
 const DEV_BASE_URL = "http://localhost:3000/";
 
-const API_URL =
-  import.meta.env.VITE_NODE_ENV === "development" ? DEV_BASE_URL : API_BASE_URL;
+const API_URL = import.meta.env.DEV ? DEV_BASE_URL : API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_URL,
